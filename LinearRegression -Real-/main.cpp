53b5bc1c1df8de6a70871b343d76e9eb35497af8
Gavin Kaepernick <kaepernick.gavin6@gmail.com>
2019-11-06 16:02:39 -0600

    Initial Commit

---
+++ LinearRegression -Real-/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LinearRegression -Real-
+//
+//  Created by Gavin Kaepernick on 11/6/19.
+//  Copyright © 2019 Gavin Kaepernick. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
